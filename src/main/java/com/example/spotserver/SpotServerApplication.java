f21856e5c901b35f8628c7fba89c7674cdc7da4d
LSM <dltmdals6718@naver.com>
2023-11-06 11:32:08 +0900

    init

---
+++ src/main/java/com/example/spotserver/SpotServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.spotserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpotServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spotserver/SpotServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spotserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

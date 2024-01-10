cc67f4abf2b9ab641186893d6b418ffbe5fa8fef
LSM <dltmdals6718@naver.com>
2024-01-10 17:13:42 +0900

    Location table 생성

--- src/main/resources/table.sql
+++ src/main/resources/table.sql
@@ -32,5 +32,13 @@ CREATE TABLE image_file (
     FOREIGN KEY (inquiry_id) REFERENCES inquiry(id)
 )
 
+CREATE TABLE location_image (
+                            id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                            location_id BIGINT,
+                            upload_file_name VARCHAR(255),
+                            store_file_name VARCHAR(255),
+                            FOREIGN KEY (location_id) REFERENCES location(id)
+)
+
 INSERT INTO location(latitude, longitude, title, address, description) VALUES(35.24308, 128.6934, '창원대학교 운동장', '의창구 창원대학로 20 창원시 경상 남도', '낭만 가득 운동장');
 INSERT INTO location(latitude, longitude, title, address, description) VALUES(35.23296, 128.6805, '용지호수공원', '경상남도 창원시 의창구 용지동 551-4', '용지호수는 경상남도 창원시 성산구 용지동에 있는 호수이다. 창원시를 대표하는 호수이며, 용지공원 안에 있다.');
